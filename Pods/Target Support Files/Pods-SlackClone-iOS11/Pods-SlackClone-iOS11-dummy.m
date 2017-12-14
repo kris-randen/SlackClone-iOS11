e2c4714409240a2fec3e9b734dea85d8f94748ac
Kris Rajendren <kris.randen@gmail.com>
2017-12-13 21:04:01 -0500

    Alamofire SwiftyJSON and Socket.IO-Client-Swift Pods Installed and Built Project Successfully.

---
+++ Pods/Target Support Files/Pods-SlackClone-iOS11/Pods-SlackClone-iOS11-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SlackClone_iOS11 : NSObject
+@end
+@implementation PodsDummy_Pods_SlackClone_iOS11
+@end
---
+++ Pods/Target Support Files/Pods-SlackClone-iOS11/Pods-SlackClone-iOS11-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SlackClone_iOS11VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SlackClone_iOS11VersionString[];
+
